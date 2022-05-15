e8cf972b7a4ac6264237c9eb9b5f0c8293edab1b
mycahh <mikaelfernandezj.d@gmail.com>
2022-05-15 14:34:00 -0400

    added React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <iframe width="560" height="315" src="https://www.youtube.com/embed/0yqm7vrCp-g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+}
+
+export default App
